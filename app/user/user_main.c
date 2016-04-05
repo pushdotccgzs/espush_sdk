8d2f84a6d157f72ef7ac6dd7ae6ea1ebb5e7ccc6
espush <webmaster@espush.cn>
2016-04-05 21:21:20 +0800

    user_main example bug fixed, thx 雪霁.

--- app/user/user_main.c
+++ app/user/user_main.c
@@ -16,101 +16,13 @@
 #include <mem.h>
 #include <espconn.h>
 
-
-static struct espconn user_conn;
-static esp_tcp user_tcp;
-static os_timer_t gl_timer;
-static os_timer_t wifi_checker;
-
-
-void init_done_cb(void* param);
+#include "push.h"
 
 
 void ICACHE_FLASH_ATTR user_rf_pre_init()
 {
 	os_printf("user_rf_pre_init.\n");
 }
-
-
-void reconnect_timer_cb(void* param)
-{
-	os_printf("reconnect_timer_cb\n");
-	init_done_cb(NULL);
-}
-
-
-void reconnect()
-{
-	os_timer_disarm(&gl_timer);
-	os_timer_setfn(&gl_timer, reconnect_timer_cb, NULL);
-	os_timer_arm(&gl_timer, 1000, 0);
-}
-
-
-void disconnected_cb(void* args)
-{
-	os_printf("DISCONNECTED.\n");
-	reconnect();
-}
-
-
-void recv_cb(void* args, char *pdata, unsigned short len)
-{
-	os_printf("RECV CB: [%d], [%d].\n", len, sizeof(unsigned short));
-}
-
-
-void sent_cb(void* args)
-{
-	os_printf("SENT.\n");
-}
-
-
-void connected_cb(void* args)
-{
-	os_printf("CONNECTED.\n");
-    espconn_regist_disconcb(&user_conn, disconnected_cb);
-    espconn_regist_recvcb(&user_conn, recv_cb);
-    espconn_regist_sentcb(&user_conn, sent_cb);
-
-    espconn_sent(&user_conn, "HELLO", os_strlen("HELLO"));
-}
-
-
-void reconnected_cb(void* args, sint8 err)
-{
-	os_printf("RECONNECTED [%d].\n", err);
-	reconnect();
-}
-
-
-void init_done_cb(void* param)
-{
-	if(wifi_station_get_connect_status() != STATION_GOT_IP) {
-		os_printf("WAIT...\n");
-		return;
-	} else {
-		os_timer_disarm(&wifi_checker);
-	}
-
-
-	user_conn.type = ESPCONN_TCP;
-	user_conn.state = ESPCONN_NONE;
-	user_conn.proto.tcp = &user_tcp;
-	user_conn.proto.tcp->remote_ip[0] = 192;
-	user_conn.proto.tcp->remote_ip[1] = 168;
-	user_conn.proto.tcp->remote_ip[2] = 0;
-	user_conn.proto.tcp->remote_ip[3] = 103;
-	user_conn.proto.tcp->local_port = espconn_port();
-	user_conn.proto.tcp->remote_port = 12345;
-
-	//regist event callback
-    espconn_regist_connectcb(&user_conn, connected_cb);
-    espconn_regist_reconcb(&user_conn, reconnected_cb);
-
-    espconn_connect(&user_conn);
-}
-
 
 /******************************************************************************
  * FunctionName : user_init
@@ -125,16 +37,14 @@ void ICACHE_FLASH_ATTR user_init(void)
 	struct rst_info* rstInfo = system_get_rst_info();
 
 	struct station_config config;
-	os_strcpy(config.ssid, "ChinaNet-966");
-	os_strcpy(config.password, "Jchen0406");
+	os_strcpy(config.ssid, "TEST8266");
+	os_strcpy(config.password, "1jcsxeastl");
 
 	wifi_set_opmode(STATION_MODE);
 	wifi_station_set_config(&config);
 	wifi_station_set_auto_connect(1);
 	wifi_station_dhcpc_start();
 
-	os_timer_disarm(&wifi_checker);
-	os_timer_setfn(&wifi_checker, init_done_cb, NULL);
-	os_timer_arm(&wifi_checker, 1000, 1);
+	//uint32 appid, char appkey[32], char devid[32], enum VERTYPE type, msg_cb msgcb
+	espush_register(1234, "25b28f0ffb9711e4a96d446d579b49a1", "", VER_SDK, NULL);
 }
-
